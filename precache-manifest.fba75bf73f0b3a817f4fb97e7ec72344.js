801b08bbc6ce8fc829b0334937fe3ec82e533a8c
CrimsonMax <61148415+CrimsonMax@users.noreply.github.com>
2020-12-09 22:24:27 +0300

    push

---
+++ precache-manifest.fba75bf73f0b3a817f4fb97e7ec72344.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad2c94d18a8b7e98b81abadc8f0dc690",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80f11971b120437fd4df",
+    "url": "/static/css/main.2929d17f.chunk.css"
+  },
+  {
+    "revision": "e872f15cb3ad66a51e37",
+    "url": "/static/js/2.5a7d37a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5a7d37a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80f11971b120437fd4df",
+    "url": "/static/js/main.aa9b1e02.chunk.js"
+  },
+  {
+    "revision": "5f1abbd3154a3f3a7eb5",
+    "url": "/static/js/runtime-main.f4676241.js"
+  }
+]);
